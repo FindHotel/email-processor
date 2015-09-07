bb60ab427e1821a09a09051441a339312e5ed442
German Gomez-Herrero <g@germangh.com>
2015-09-07 20:07:24 +0000

    Refactor some more

--- emailprocessor/basic.py
+++ emailprocessor/basic.py
@@ -6,6 +6,8 @@ from emailprocessor.common import BaseSMTPServer
 from emailprocessor.utils import _print
 import email.parser
 import os
+import abc
+import uuid
 
 
 class PrintSummarySMTPServer(BaseSMTPServer):
@@ -17,11 +19,7 @@ class PrintSummarySMTPServer(BaseSMTPServer):
         _print("Message length        : {}".format(len(data)))
 
 
-class SaveAttachmentsSMTPServer(BaseSMTPServer):
-    def __init__(self, directory=None, **kwargs):
-        super().__init__(**kwargs)
-        self.directory = directory
-
+class ProcessAttachmentsSMTPServer(BaseSMTPServer, metaclass=abc.ABCMeta):
     def _process(self, peer, mailfrom, rcpttos, data):
         """Saves email attachments in the specified directory"""
         parser = email.parser.Parser()
@@ -36,8 +34,22 @@ class SaveAttachmentsSMTPServer(BaseSMTPServer):
                 continue
             if not os.path.isdir(self.directory):
                 os.makedirs(self.directory)
-            # We should sanitize the filename: TO DO!
-            target_file = os.path.join(self.directory, filename)
-            with open(target_file, 'wb') as fp:
-                fp.write(part.get_payload(decode=True))
-            _print("Saved {}".format(target_file))
+
+            self._process_attachment(part.get_payload(decode=True), filename)
+
+    @abc.abstractmethod
+    def _process_attachment(self, payload, filename):
+        """To be implemented by final classes"""
+        pass
+
+
+class SaveAttachmentsSMTPServer(ProcessAttachmentsSMTPServer):
+    def __init__(self, directory=None, **kwargs):
+        super().__init__(**kwargs)
+        self.directory = directory
+
+    def _process_attachment(self, payload, filename):
+        target_file = os.path.join(self.directory, filename)
+        with open(target_file, 'wb') as fp:
+            fp.write(payload)
+        _print("Saved {}".format(target_file))
