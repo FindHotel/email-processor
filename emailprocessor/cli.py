027feda2629905c4953816e248b9f6a0deccb2d8
German Gomez-Herrero <g@germangh.com>
2015-09-11 13:12:39 +0000

    Refactoring

--- emailprocessor/cli.py
+++ emailprocessor/cli.py
@@ -7,7 +7,9 @@ from __future__ import print_function
 import click
 import emailprocessor.basic as basic
 import emailprocessor.bing as bing
+from emailprocessor import EmailServer
 import os
+import uuid
 
 
 @click.group()
@@ -16,29 +18,43 @@ import os
 @click.option('--port', '-p', default=None, type=int)
 @click.option('--debug/--no-debug', default=False)
 @click.option('--timeout', default=None, type=int)
+@click.option('--certfile', default=None, type=str)
+@click.option('--certkey', default=None, type=str)
 @click.option('--username', default=None, type=str)
-def emailprocessor(ctx, address, port, debug, timeout, username):
+def emailprocessor(ctx, address, port, debug, timeout, username, certkey,
+                   certfile):
     """Simple SMTP server for processing emails"""
-    if username is None:
-        username = os.environ.get('EMAILPROCESSOR_USERNAME')
-    if port is None:
-        port = int(os.environ.get('EMAILPROCESSOR_PORT'))
+    username = _resolve_username(username)
     if address is None:
         address = os.environ.get('EMAILPROCESSOR_ADDRESS')
-    ctx.obj = {'address': address, 'port': port, 'debug': debug,
-               'timeout': timeout, 'username': username}
+    if port is None:
+        port = int(os.environ.get('EMAILPROCESSOR_PORT'))
+    if certkey is None:
+        certkey = os.environ.get('EMAILPROCESSOR_CERTKEY')
+    if certfile is None:
+        certfile = os.environ.get('EMAILPROCESSOR_CERTFILE')
+
+    server = EmailServer(addr=(address, port), debug=debug, timeout=timeout,
+                         certfile=certfile, certkey=certkey)
+
+    ctx.obj = {'server': server, 'username': username}
+
+
+def _resolve_username(username):
+    if username is None:
+        username = os.environ.get('EMAILPROCESSOR_USERNAME')
+        if username is None:
+            username=str(uuid.uuid4())
+    return username
 
 
 @emailprocessor.command()
 @click.pass_context
 def email_summary(ctx):
     """Prints a summary of the email"""
-    with basic.PrintSummarySMTPServer(
-            (ctx.obj['address'], ctx.obj['port']),
-            debug=ctx.obj['debug'],
-            timeout=ctx.obj['timeout'],
-            username=ctx.obj['username']) as server:
-        server.run()
+    server = ctx.obj['server']
+    server.register_processor(ctx.obj['username'], basic.PrintSummary())
+    server.run()
 
 
 @emailprocessor.command()
@@ -46,13 +62,11 @@ def email_summary(ctx):
 @click.option('--directory', default='/var/email-processor')
 def save_attachments(ctx, directory):
     """Saves email attachments"""
-    server = basic.SaveAttachmentsSMTPServer(addr=(ctx.obj['address'],
-                                              ctx.obj['port']),
-                                             debug=ctx.obj['debug'],
-                                             timeout=ctx.obj['timeout'],
-                                             username=ctx.obj['username'],
-                                             directory=directory)
+    server = ctx.obj['server']
+    server.register_processor(ctx.obj['username'],
+                              basic.SaveAttachments(directory))
     server.run()
+
 
 @emailprocessor.command()
 @click.pass_context
@@ -62,13 +76,9 @@ def save_attachments(ctx, directory):
               default=os.environ.get('EMAILPROCESSOR_BING_TO_S3_BUCKET'))
 def bing_to_s3(ctx, prefix, bucket):
     """Moves Bing reports to S3"""
-    server = bing.BingReportsToS3SMTPServer(addr=(ctx.obj['address'],
-                                                  ctx.obj['port']),
-                                            debug=ctx.obj['debug'],
-                                            timeout=ctx.obj['timeout'],
-                                            username=ctx.obj['username'],
-                                            prefix=prefix,
-                                            bucket=bucket)
+    server = ctx.obj['server']
+    processor = bing.BingReportsToS3(bucket=bucket, prefix=prefix)
+    server.register_processor(ctx.obj['username'], processor)
     server.run()
 
 
