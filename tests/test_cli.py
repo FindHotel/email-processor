9359b5e0d67c1d8220cfb81eb8c209131b948a07
German Gomez-Herrero <g@germangh.com>
2015-09-25 21:31:52 +0200

    Remove test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -27,14 +27,3 @@ class TestCli():
         result = runner.invoke(emailprocessor, [helparg])
         assert result.exit_code == 0
         assert 'emailprocessor' in result.output
-
-    @parametrize('addrarg', ['--address', '-a'])
-    @parametrize('portarg', ['--port', '-p'])
-    @parametrize('debugarg', ['--debug', '--no-debug'])
-    def test_address(self, addrarg, portarg, debugarg, runner, command):
-        result = runner.invoke(
-            emailprocessor,
-            ['--timeout', 1, addrarg, '127.0.0.1',
-             portarg, 1027, debugarg, command])
-        assert result.exit_code == 0
-        assert '127.0.0.1' in result.output
